bcaafce93e7305473a8d84f2d82ce2d99a3e1746
James McLachlan <claquer@gmail.com>
2015-02-05 21:41:12 -0500

    Revert "Now with AmqpClient"
    This reverts commit 21f400d18d7216bd3b254968c664554e9c8edfa5.

--- Proto/Proto.cpp
+++ Proto/Proto.cpp
@@ -1,14 +1,11 @@
 #include <string>
 #include <iostream>
 #include <fstream>
-#include <SimpleAmqpClient/SimpleAmqpClient.h>
 #include "JobConfig.pb.h"
 using namespace std;
 
 int main()
 {
-	boost::shared_ptr<int> myint;
-	AmqpClient::Channel::ptr_t connection = AmqpClient::Channel::Create("localhost");
 	GetJobConfigRequest request;
 	fstream input("..\\request.bin", ios::in | ios::binary);
 	if (!input)
